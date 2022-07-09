180108cd48cd0b75ddcc4d93e3dd998ca562d6c4
Brian Samuels <samuels.brian9@gmail.com>
2022-07-09 11:29:29 -0400

    adding in server side folders and files

---
+++ server/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
+const { authMiddleware } = require("./utils/auth");
+
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
+// const models = require("./models");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`\nAPI server running on port ${PORT}!`);
+      console.log(
+        `\nUse GraphQL at http://localhost:${PORT}${server.graphqlPath} \n`
+      );
+    });
+  });
+};
+
+startApolloServer(typeDefs, resolvers);
